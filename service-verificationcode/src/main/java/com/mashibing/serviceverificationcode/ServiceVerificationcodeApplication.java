165bb6ba1151309aee6c02078659da07bfd023c5
dyj3344 <535277189@qq.com>
2023-05-06 11:00:32 +0800

    nacos注册

--- service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/ServiceVerificationcodeApplication.java
+++ service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/ServiceVerificationcodeApplication.java
@@ -1,4 +1,4 @@
-package com.mashibing.apipassenger;
+package com.mashibing.serviceverificationcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiPassengerApplication {
+public class ServiceVerificationcodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiPassengerApplication.class);
+        SpringApplication.run(ServiceVerificationcodeApplication.class, args);
     }
+
 }
